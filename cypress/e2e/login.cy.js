111efa5ad0c6de2ec103306a12c25265daa5adcc
Abraham Arias <arias.abram@gmail.com>
2023-10-24 22:50:20 -0700

    initial set up

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,20 +1,30 @@
 describe('login functionality', () => {
     beforeEach(() => {
-        cy.visit('https://www.saucedemo.com/');
+        cy.visit('/practice-test-login');
       })
-    it('should not be able to login with incorrect credentials', () => {
-        cy.get('[data-test="username"]').type("standard_user")
-        cy.get('[data-test="password"]').type("wrong password")
-        cy.get('[data-test="login-button"]').click();
+    it('should be able to login with correct credentials', () => {
+        //#using cypress fixtures to get user and password info.
+        cy.fixture("users").then((fixtureData) => {
+            const username = fixtureData.username;
+            const password = fixtureData.password;
 
-        cy.get('[data-test="error"]').should('contain', 'Username and password do not match any user in this service')
+            cy.get('#username').type(username);
+            cy.get('#password').type(password);
+            cy.get('#submit').click();
+
+            cy.get('h1.post-title').should('have.text', 'Logged In Successfully');
+        })
     })
-    it('should be able to login with standard user', () => {
-        cy.get('[data-test="username"]').type("standard_user")
-        cy.get('[data-test="password"]').type("secret_sauce")
-        cy.get('[data-test="login-button"]').click();
+    it('should not be able to login with incorrect username', () => {
+        //#using cypress custom command to login.
+        cy.login('incorrectUser','Password123');
 
-        cy.url().should('include', '/inventory.html')
-        cy.get('span.title').should('contain', 'Products')
+        cy.get('div#error').should('have.text', 'Your username is invalid!');
+    })
+    it('should not be able to login with incorrect password', () => {
+        //#using cypress custom command to login.
+        cy.login('student','incorrectPassword');
+
+        cy.get('div#error').should('have.text', 'Your password is invalid!');
     })
   })
